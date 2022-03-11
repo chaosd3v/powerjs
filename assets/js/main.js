a5be31e6fa2289d28e2c1d16665285a9916ac154
chaosd3v <saurabhjg101@gmail.com>
2022-03-11 14:21:13 +0530

    Refactored & added device navigator info & compatibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,41 @@
-const batteryPercent = document.getElementById("batteryPercent")
-const batteryStatus = document.getElementById("batteryStatus")
+const batteryPercent = document.getElementById("batteryPercent");
+const batteryStatus = document.getElementById("batteryStatus");
+const browser = document.getElementById("browser");
+const platform = document.getElementById("platform");
+const notSupported = document.getElementById("notSupported");
+const deviceContent = document.getElementById("deviceContent");
 
-showBatteryInfo()
+showDeviceInfo();
 
-function showBatteryInfo() {
+async function showDeviceInfo() {
     //Get battery info 
-    navigator.getBattery()
-    .then(function(battery) {
-        const level = battery.level * 100
-        batteryPercent.textContent = level + " %"
-        batteryStatus.textContent = (battery.charging) ? "Charging" : "On Battery"
-    });
+    const isSupported = navigator && 'getBattery' in navigator
+    if (isSupported) {
+        navigator.getBattery()
+            .then(function (battery) {
+
+                battery.addEventListener('levelchange', () => {
+                    updateBatteryInfo(battery);
+                });
+
+                battery.addEventListener('chargingchange', () => {
+                    updateBatteryInfo(battery);
+                });
+                updateBatteryInfo(battery);
+
+            });
+
+        browser.textContent = navigator.userAgentData.brands[2].brand
+        platform.textContent = navigator.userAgentData.platform
+    }else {
+        deviceContent.classList.add("hide");
+        notSupported.classList.remove("hide");
+    }
+
+}
+
+function updateBatteryInfo(battery) {
+    const level = battery.level * 100;
+    batteryPercent.textContent = level + " %";
+    batteryStatus.textContent = (battery.charging) ? "Charging" : "On Battery";
 }
